b193fc1ef6fd4645c2c014e5c51de2b27ad9ddb9
Matthijs <matthijs.vannistelrooij@gmail.com>
2025-01-04 18:22:53 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,63 @@
+import { Canvas } from "@react-three/fiber/native"
+import { StatusBar } from "expo-status-bar"
+import { Suspense, useState } from "react"
+import { StyleSheet, Text, View } from "react-native"
+import { BottomUI } from "./components/BottomUI"
+
+import Loading from "./components/Loading"
+import { Z1000 } from "./components/Z1000"
+
+export default function App() {
+  const [sliderValue, setSliderValue] = useState(1.3)
+  const [selectedColor, setSelectedColor] = useState("#D76C6C")
+
+  const handleSliderValueChange = (value) => {
+    setSliderValue(value)
+    console.log(`Parent received slider value: ${value}`)
+  }
+
+  const handleColorChange = (color) => {
+    setSelectedColor(color)
+  }
+
+  return (
+    <View style={styles.container}>
+      <Canvas camera={{ position: [-2, 2.5, 5], fov: 30 }}>
+        <color attach="background" args={["#3d3d3d"]} />
+        <ambientLight intensity={1} />
+        <directionalLight position={[5, 5, 5]} intensity={5} />
+        <Suspense fallback={<Loading />}>
+          <Z1000 sliderValue={sliderValue} color={selectedColor} />
+        </Suspense>
+      </Canvas>
+
+      <BottomUI
+        onSliderValueChange={handleSliderValueChange}
+        onColorChange={handleColorChange}
+      />
+      <StatusBar style="auto" />
+      <View style={[styles.buttonContainer]}>
+        <Text style={[styles.text, { color: selectedColor }]}>
+          Kawasaki Z1000
+        </Text>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#aaa",
+  },
+  buttonContainer: {
+    position: "absolute",
+    top: 50,
+    alignSelf: "center",
+    padding: 10,
+    borderRadius: 8,
+  },
+  text: {
+    fontSize: 40,
+  },
+})
---
+++ components/BottomUI.jsx
@@ -0,0 +1,99 @@
+import { SafeAreaView, Text, View, TouchableOpacity } from "react-native"
+import { useState } from "react"
+import Slider from "@react-native-community/slider"
+import { StyleSheet } from "react-native"
+
+export const BottomUI = ({ onSliderValueChange, onColorChange }) => {
+  const colors = ["#D76C6C", "#6CD76C", "#6C6CD7", "#D7D76C", "#D76CD7"]
+  const [sliderValue, setSliderValue] = useState(1.3)
+
+  const handleColorChange = (color) => {
+    console.log(`Selected color: ${color}`)
+    onColorChange(color)
+  }
+
+  const handleSliderChange = (value) => {
+    setSliderValue(value)
+    console.log(`Slider value: ${value}`)
+    onSliderValueChange(value)
+  }
+
+  return (
+    <SafeAreaView edges={["bottom"]} style={styles.safeArea}>
+      <View style={styles.sliderContainer}>
+        <View style={styles.sliderWrapper}>
+          <Slider
+            style={styles.slider}
+            minimumValue={1.2}
+            maximumValue={4.2}
+            step={0.1}
+            value={sliderValue}
+            onValueChange={handleSliderChange}
+            minimumTrackTintColor="#ffffff"
+            maximumTrackTintColor="#ffffff"
+            thumbTintColor="#a2a2a2"
+            thumbStyle={styles.thumb}
+            trackStyle={styles.track}
+          />
+        </View>
+
+        <View style={styles.colorPicker}>
+          {colors.map((color, index) => (
+            <TouchableOpacity
+              key={index}
+              onPress={() => handleColorChange(color)}
+              style={[styles.colorOption, { backgroundColor: color }]}
+            />
+          ))}
+        </View>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  safeArea: {
+    backgroundColor: "#7a7a7a",
+    height: 250
+  },
+  sliderContainer: {
+    padding: 40,
+    alignItems: "center",
+  },
+  sliderWrapper: {
+    width: "100%",
+    marginBottom: 40,
+    alignItems: "center",
+  },
+  slider: {
+    width: "100%",
+    height: 20,
+  },
+  thumb: {
+    width: 20,
+    height: 20,
+    borderRadius: 4,
+    borderColor: "#ffffff",
+    borderWidth: 1,
+  },
+  track: {
+    borderRadius: 5,
+  },
+  colorPicker: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 12,
+    marginBottom: 20,
+  },
+  colorOption: {
+    width: 25,
+    height: 25,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 2,
+    borderColor: "#000000",
+    margin: 15,
+  },
+})
